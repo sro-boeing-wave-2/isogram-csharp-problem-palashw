c183c3a03e0706ec496af31498276d8f3ac18f32
Palash <palashw173@gmail.com>
2018-07-21 10:42:54 +0530

    Isogram done

---
+++ isogram-csharp-problem/isogram-csharp-problem/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace isogram_csharp_problem
+{
+    public class isogram
+    {
+        public static int Check_isogram (string str)
+        {
+            str = str.ToLower();
+            char[] sortstr = str.ToCharArray();
+
+            Array.Sort(sortstr);
+
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                if (sortstr[i] == sortstr[i + 1])
+                {
+                    return 0;
+                }
+            }
+            return 1;
+ 
+        }
+    }
+}
